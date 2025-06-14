b260a23b3d6a45f284e81d1d5741bcb8b8d2f434
bruno <bruno.golinf@gmail.com>
2025-06-14 16:19:05 -0300

    Adaptacoes para outra Planilha

--- produtos/models.py
+++ produtos/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
-# Create your models here.
-
 class Produto(models.Model):
-    SKU = models.IntegerField()
-    Produto = models.TextField()
-    Dados_Complementares = models.TextField()
-    UM = models.CharField(max_length=200)
-    Categoria = models.TextField()
+    loja = models.CharField(max_length=100, null=True, blank=True)
+    endereco1 = models.CharField(max_length=200, null=True, blank=True)
+    cidade = models.CharField(max_length=100, null=True, blank=True)
+
+    uniformes = models.IntegerField(null=True, blank=True)
+    vol_unif = models.FloatField(null=True, blank=True)
+
+    cam_bone = models.IntegerField(null=True, blank=True)
+    vol_cb = models.FloatField(null=True, blank=True)
+
+    vol = models.FloatField(null=True, blank=True)
+    peso = models.FloatField(null=True, blank=True)
+
+    nota = models.CharField(max_length=50, null=True, blank=True)
+    emite_nf = models.CharField(max_length=10, null=True, blank=True)
+
+
 
     def __str__(self):
             return self.Produto
--- produtos/views.py
+++ produtos/views.py
@@ -6,32 +6,54 @@ from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 
-# Create your views here.
-
+# Mapear os cabeçalhos do Excel para os campos do model Produto
+CAMPOS_IMPORTAR = {
+    'Loja': 'loja',
+    'Endereço1': 'endereco1',
+    'Cidade': 'cidade',
+    'Uniformes': 'uniformes',
+    'Vol. Unif.': 'vol_unif',
+    'Cam./bone': 'cam_bone',
+    'Vol. C / B': 'vol_cb',
+    'Vol.': 'vol',
+    'Peso': 'peso',
+    'Nota': 'nota',
+    'Emite NF': 'emite_nf',
+}
 
 def importar_produtos(request):
-    # Inicializa o formulário para a requisição GET ou se o formulário estiver inválido no POST
     form = UploadExcelForm()
 
     if request.method == 'POST':
         form = UploadExcelForm(request.POST, request.FILES)
         if form.is_valid():
             arquivo_excel = request.FILES['arquivo_excel']
-            wb = load_workbook(arquivo_excel)
+            wb = load_workbook(arquivo_excel, data_only=True)
             sheet = wb.active
 
-            # Limpa os produtos antigos (opcional)
             Produto.objects.all().delete()
 
-            # Itera pelas linhas da planilha e cria os produtos
+            cabecalho = [cell.value for cell in sheet[1]]
+
+            # Mapeia campo_model --> índice na planilha
+            campos_indices = {}
+            for idx, col_name in enumerate(cabecalho):
+                if col_name in CAMPOS_IMPORTAR:
+                    campos_indices[CAMPOS_IMPORTAR[col_name]] = idx
+
+            campos_model_necessarios = set(CAMPOS_IMPORTAR.values())
+            campos_faltando = campos_model_necessarios - set(campos_indices.keys())
+            if campos_faltando:
+                return render(request, 'produtos/importar_produtos.html', {
+                    'form': form,
+                    'erro': f"Colunas obrigatórias não encontradas: {', '.join(campos_faltando)}"
+                })
+
             for row in sheet.iter_rows(min_row=2, values_only=True):
-                Produto.objects.create(
-                    SKU=row[0],
-                    Produto=row[1],
-                    Dados_Complementares=row[2],
-                    UM=row[3],
-                    Categoria=row[4],
-                )
+                kwargs = {}
+                for campo, idx in campos_indices.items():
+                    kwargs[campo] = row[idx]
+                Produto.objects.create(**kwargs)
 
             return redirect('lista_produtos') 
         
@@ -40,35 +62,31 @@ def importar_produtos(request):
 def lista_produtos(request):
     produtos_list = Produto.objects.all()
 
-    # Filtragem
     search_query = request.GET.get('search', '')
     if search_query:
         produtos_list = produtos_list.filter(
-            Produto__icontains=search_query
+            loja__icontains=search_query
         )
 
-    # Ordenação
-    sort_by = request.GET.get('sort', 'Produto')  # Ordena por Produto por padrão
+    sort_by = request.GET.get('sort', 'loja')
     order_direction = request.GET.get('direction', 'asc')
 
     if order_direction == 'desc':
-        sort_by = f'-{sort_by}'  # Adiciona o '-' para ordem decrescente
+        sort_by = f'-{sort_by}'
 
     produtos_list = produtos_list.order_by(sort_by)
 
-    # Paginação
-    paginator = Paginator(produtos_list, 150)  # Exibe 150 produtos por página
+    paginator = Paginator(produtos_list, 150)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    
-    # Envia a direção atual para o template
+
     context = {
         'page_obj': page_obj,
         'search_query': search_query,
         'sort_by': sort_by.lstrip('-'),
         'order_direction': order_direction,
     }
-    
+
     return render(request, 'produtos/lista_produtos.html', context)
 
 def imprimir_produtos(request):
@@ -76,12 +94,7 @@ def imprimir_produtos(request):
         produtos_ids = request.POST.getlist('produtos_selecionados')
         if produtos_ids:
             produtos = Produto.objects.filter(id__in=produtos_ids)
-            
-            # Gerar o conteúdo para impressão
             html_content = render_to_string('produtos/imprimir_produtos.html', {'produtos': produtos})
-            
-            # Retorna uma página de visualização para impressão
             return render(request, 'produtos/imprimir_produtos.html', {'produtos': produtos})
-        
-    # Redireciona para a lista de produtos se nada for selecionado
+
     return redirect('lista_produtos')
